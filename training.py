2118ba39222095beb3b6ec2648aa0ac3c277f862
IOANNIS ROUMELIOTIS <it2021087@hua.gr>
2024-04-06 14:49:28 +0300

    made basic intents for harokopio bot and basic training and chat

---
+++ training.py
@@ -0,0 +1,81 @@
+import random
+import json
+import pickle
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
+
+# Initialize WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+# Load intents from JSON file
+intents = json.loads(open('intents.json').read())
+
+# Initialize lists for words, classes, documents
+words = []
+classes = []
+documents = []
+ignore_letters = ['!', '?', ',', '.']
+
+# Preprocess intents and create word list, document list, and class list
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+# Lemmatize words and remove ignore letters
+words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in ignore_letters]
+words = sorted(set(words))
+classes = sorted(set(classes))
+
+# Save words and classes to pickle files
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+# Create training data
+training = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+
+# Shuffle training data
+random.shuffle(training)
+
+# Split training data into input and output arrays
+train_x = np.array([pattern[0] for pattern in training])
+train_y = np.array([pattern[1] for pattern in training])
+
+# Define the model architecture
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# Compile the model
+sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# Train the model
+hist = model.fit(train_x, train_y, epochs=200, batch_size=5, verbose=1)
+
+# Save the model
+model.save('chatbot_model.h5')
+
+print('Done')
