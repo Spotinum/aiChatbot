b78ea1756c1dccc67dde2041f22172dc8231110e
IOANNIS ROUMELIOTIS <giaroumel@gmail.com>
2024-04-12 00:41:51 +0300

    added spring project, made a relational model in directory diagrams

---
+++ aiChatBotSpring/src/test/java/dit/hua/aiChatBot/AiChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package dit.hua.aiChatBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiChatBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
